a62a19fec9a3471b1979d7b7a5a82e1f7d6f67a4
prayogo17 <nugrohoprayogo97@gmail.com>
2020-03-15 21:20:32 +0700

    Update javabaru.js

--- javabaru.js
+++ javabaru.js
@@ -6,7 +6,7 @@
       for (var r = 0; r < a.length; ++r) {
           var n = e.feed.entry[a[r]].link[4].href,
               l = e.feed.entry[a[r]].link[4].title;
-          if ($("#read-more ul").append('<li><a href="' + n + '">' + l + "</a></li>"), jml_artikel-1 == r) break
+          if ($(".read-more ul").append('<li><a href="' + n + '">' + l + "</a></li>"), jml_artikel-1 == r) break
       }
   }
 
