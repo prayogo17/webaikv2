8b365f8cbc9e86b157cf32906685dc060d1923e5
prayogo17 <nugrohoprayogo97@gmail.com>
2020-07-27 16:42:06 +0700

    Update java135.js

--- java135.js
+++ java135.js
@@ -44,7 +44,7 @@ $.fn.isInViewport = function() {
 
 
 function lazy_load() {
-  $("#konten-container").scroll(function(event) {
+  $(window).scroll(function(event) {
 if(img.length!==0){
     img.forEach(function(item, i) {
 
